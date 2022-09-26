bb11bb3cc84e81a0bcbc87cbc33badcf7f390254
David Fragua <davidfragua@gmail.com>
2022-09-26 19:31:19 +0200

    Solved lab

--- src/movies.js
+++ src/movies.js
@@ -1,16 +1,77 @@
+console.log("probando");
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors(moviesArray) {}
+function getAllDirectors(moviesArray) {
+  let cloneMoviesArray = structuredClone(moviesArray);
+  let directors = cloneMoviesArray.map((eachMovie) => {
+    return eachMovie.director;
+  });
+  return directors;
+}
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies(moviesArray) {}
+function howManyMovies(moviesArray) {
+    if (moviesArray === 0) {
+        return 0
+    }
+    
+  let filteredMovies = moviesArray.filter((eachMovie) => {
+    if (
+      eachMovie.director === "Steven Spielberg" &&
+      eachMovie.genre.includes("Drama")
+    ) {
+      return true
+    } else {
+      return false;
+    }
+  });
+  return filteredMovies.length
+}
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage(moviesArray) {}
+function scoresAverage(moviesArray) {
+    if (moviesArray.length === 0){
+        return 0
+    }
+    
+  let scoreTotal = moviesArray.reduce((acc, eachElement) => {
+    if (eachElement.score === undefined ){
+        return acc;
+    } else {
+    return acc + eachElement.score;
+    }
+  }, 0);
+//  haciendo un console.log salen 4 valores pero no logro averiguar por qué
+  
+  let average = scoreTotal / moviesArray.length
+  return Number(average.toFixed(2))
+
+}
+
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore(moviesArray) {}
+function dramaMoviesScore(moviesArray) {
+    if (moviesArray.length === undefined){
+        return 0;
+    } 
+    let dramasTotales = moviesArray.map = ((eachMovie) =>{
+        if (eachMovie.genre.includes("Drama")){
+            return true
+        }
+       return dramasTotales
+    })
+    let scoreDramas = dramasTotales.reduce((acc, eachElement) =>{
+        if (eachElement.score === undefined){
+            return acc;
+        } else {
+            return acc + eachElement.score
+        }
+    },0);
+    return scoreDramas / dramasTotales.length
+    // console.log(scoreDramas / dramasTotales.length)
+
+} 
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 function orderByYear(moviesArray) {}
